cea84c0a3c5d4f96ecd8d92ee631773fa8907a91
trip6love <lanebyers13@gmail.com>
2021-11-09 20:22:06 -0500

    Added Java Vars and Fixed HTMLclass error

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,4 @@
+// Main content //
 var boxStartEl = document.getElementById("home-box");
 var boxQuestionEl = document.getElementById("question-box");
 var boxEndEl = document.getElementById("end-box");
@@ -8,3 +9,24 @@ var highScoreLsEl = document.getElementById("high-score-list");
 var boxScoreEl = document.getElementById("score-banner");
 var incorrectEl = document.getElementById("incorrect");
 var correctEl = document.getElementById("correct");
+
+// Questions & Answers //
+var questionsEl = document.getElementById("question");
+var answerbuttonEl = document.getElementById("answer-buttons");
+var timeEl = document.querySelector("#time")
+timeEl.innerText = 0;
+var points = 0;
+var remainingTime;
+var gameOver;
+
+// Button //
+var btnClearRecord = document.querySelector("#clear-records");
+var btnBack = document.querySelector("#back");
+var btnStart = document.querySelector("#start-quiz")
+
+// High Score & Questions Array //
+var records = [];
+var randomQuestions
+var questionIndex = 0
+
+// Game Questions //
