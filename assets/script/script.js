4a03c3cd4fd681e970e20bad858910c762d198bd
trip6love <lanebyers13@gmail.com>
2021-11-20 14:10:22 -0500

    Reorganized and added comments now i need to add questions

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,310 +1,334 @@
-// Main content //
+// MAIN CONTENT //
 var boxStartEl = document.getElementById("home-box");
 var boxQuestionEl = document.getElementById("question-box");
-var boxEndEl = document.getElementById("end-box");
-var nameForm = document.getElementById("name-form");
-var boxHighScoreEl = document.getElementById("high-score-box");
-var viewHighScoreEl = document.getElementById("high-scores");
-var highScoreLsEl = document.getElementById("high-score-list");
-var boxScoreEl = document.getElementById("score-banner");
-var incorrectEl = document.getElementById("incorrect");
-var correctEl = document.getElementById("correct");
+var boxScoreEl = document.getElementById("score-tag")
+var nameForm = document.getElementById("name-form")
+var boxHighScoresEl = document.getElementById("high-score-box")
+var viewHighScoreEl = document.getElementById("high-scores")
+var lsHighScoreEl = document.getElementById("high-score-list")
+var correctEl = document.getElementById("correct")
+var incorrectEl = document.getElementById("incorrect")
+var boxEndEl = document.getElementById("end-box")
 
-// Questions & Answers //
-var questionsEl = document.getElementById("question");
-var answerbuttonEl = document.getElementById("answer-buttons");
-var timeEl = document.querySelector("#time")
-timeEl.innerText = 0;
+// QUESTIONS //
+var questionsEl = document.getElementById("question")
+var answerbuttonEl = document.getElementById("answer-buttons")
+var timeEl = document.querySelector("#time");
 var points = 0;
 var remainingTime;
-var gameOver;
+var gameover;
+timeEl.innerText = 0;
 
-// Button //
-var btnClearRecord = document.querySelector("#clear-records");
-var btnBack = document.querySelector("#back");
-var btnStart = document.querySelector("#start-quiz")
+// BUTTONS //
+var btnStartEl = document.querySelector("#start-quiz");
+var btnBackEl = document.querySelector("#back")
+var btnClearRecord = document.querySelector("#clear-records")
 
-// High Score & Questions Array //
-var records = [];
+// QUESTION ARRAY //
 var randomQuestions
 var questionIndex = 0
 
-// Game Questions //
+// RECORDS //
+var records = [];
+
+    
+// GAME QUESTIONS //
 var questions = [
-    {
-       q: "",
-       a: "",
-       options: [{option: "1. "}, {option: "2. "}, {option: "2. "}, {option: "3. "}, {option: "4. "}]
+    { 
+        q: 'To link JavaScript which would be used?', 
+        a: '2. <script>', 
+        choices: [{choice: '1. <link>'}, {choice: '2. <script>'}, {choice: '3. <js>'}, {choice: '4. <href>'}]
     },
-    {
-        q: "",
-        a: "",
-        options: [{option: "1. "}, {option: "2. "}, {option: "2. "}, {option: "3. "}, {option: "4. "}]
+    { 
+        q: 'To link JavaScript which would be used?', 
+        a: '2. <script>', 
+        choices: [{choice: '1. <link>'}, {choice: '2. <script>'}, {choice: '3. <js>'}, {choice: '4. <href>'}]
     },
-    {
-        q: "",
-        a: "",
-        options: [{option: "1. "}, {option: "2. "}, {option: "2. "}, {option: "3. "}, {option: "4. "}]
+    { 
+        q: 'To link JavaScript which would be used?', 
+        a: '2. <script>', 
+        choices: [{choice: '1. <link>'}, {choice: '2. <script>'}, {choice: '3. <js>'}, {choice: '4. <href>'}]
     },
-    {
-        q: "",
-        a: "",
-        options: [{option: "1. "}, {option: "2. "}, {option: "2. "}, {option: "3. "}, {option: "4. "}]
+    { 
+        q: 'To link JavaScript which would be used?', 
+        a: '2. <script>', 
+        choices: [{choice: '1. <link>'}, {choice: '2. <script>'}, {choice: '3. <js>'}, {choice: '4. <href>'}]
     },
-    {
-        q: "",
-        a: "",
-        options: [{option: "1. "}, {option: "2. "}, {option: "2. "}, {option: "3. "}, {option: "4. "}]
+    { 
+        q: 'To link JavaScript which would be used?', 
+        a: '2. <script>', 
+        choices: [{choice: '1. <link>'}, {choice: '2. <script>'}, {choice: '3. <js>'}, {choice: '4. <href>'}]
     },
-    {
-        q: "",
-        a: "",
-        options: [{option: "1. "}, {option: "2. "}, {option: "2. "}, {option: "3. "}, {option: "4. "}]
+    { 
+        q: 'To link JavaScript which would be used?', 
+        a: '2. <script>', 
+        choices: [{choice: '1. <link>'}, {choice: '2. <script>'}, {choice: '3. <js>'}, {choice: '4. <href>'}]
     },
-    {
-        q: "",
-        a: "",
-        options: [{option: "1. "}, {option: "2. "}, {option: "2. "}, {option: "3. "}, {option: "4. "}]
+    { 
+        q: 'To link JavaScript which would be used?', 
+        a: '2. <script>', 
+        choices: [{choice: '1. <link>'}, {choice: '2. <script>'}, {choice: '3. <js>'}, {choice: '4. <href>'}]
     },
-    {
-        q: "",
-        a: "",
-        options: [{option: "1. "}, {option: "2. "}, {option: "2. "}, {option: "3. "}, {option: "4. "}]
-    },
-
 ];
-
-var startPage = function() {
-    boxHighScoreEl.classList.add("off");
-    boxHighScoreEl.classList.remove("on");
-    boxStartEl.classList.add("on");
-    boxStartEl.classList.add("off");
+      
+// HOME SCREEN //
+var startPage = function () {
+    boxHighScoresEl.classList.add("off")
+    boxHighScoresEl.classList.remove("on")
+    boxStartEl.classList.remove("off")
+    boxStartEl.classList.add("on")
     boxScoreEl.removeChild(boxScoreEl.lastChild)
-    points = 0
-    timeEl.textContent = 0
     questionIndex = 0
-    gameOver = ""
+    gameover = ""
+    timeEl.textContent = 0 
+    points = 0
 
     if (correctEl.className = "on") {
-        correctEl.classList.add("off");
         correctEl.classList.remove("on");
-    
-    } 
-    if (wrongEl.className = "show") {
-         
-        wrongEl.classList.remove("on");
-        wrongEl.classList.add("off");
-        
+        correctEl.classList.add("off")
     }
-    
-};
+    if (incorrectEl.className = "on") {
+        incorrectEl.classList.remove("on");
+        incorrectEl.classList.add("off");
+    }
+}
 
-// Setting Up The Timer //
-var timeLeft = function() {
+// SETTING UP TIMER // 
+var timeSet = function() {
     timeleft = 60;
 
     var checkTime = setInterval(function() {
         timeEl.innerText = timeleft;
         timeleft--
 
-        if (gameOver) {
+        if (gameover) {
             clearInterval(checkTime)
         }
+       
         if (timeleft < 0) {
-            viewPoints()
-            timerEl.innerText = 0
+            showScore()
+            timeEl.innerText = 0
             clearInterval(checkTime)
         }
 
-        
-    }, 1000);
-};
+    }, 1000)
+}
 
-// Starting The Game //
+// STARTING THE GAME //
 var gameStart = function() {
-    boxStartEl.classList.remove("on");
-    boxStartEl.classList.add("off");
-    boxQuestionEl.classList.add("on");
-    boxQuestionEl.classList.remove("off")
-
-    // Randomize The Questions //
-    randomQuestions = questions.sort(() => Math.random() - 0.5);
-    timeLeft();
+    
+    boxStartEl.classList.add('off');
+    boxStartEl.classList.remove('on');
+    boxQuestionEl.classList.remove('off');
+    boxQuestionEl.classList.add('on');
+    
+    randomQuestions = questions.sort(() => Math.random() - 0.5)
+    timeSet();
     setQuestion();
-};
-
-// Next Questions //
+}
+    
+// NEXT QUESTIONS //
 var setQuestion = function() {
-    resetSolved()
+    resetSolved();
     displayQuestion(randomQuestions[questionIndex])
-};
+}
 
-// Remove Buttons //
-var resetSolved = function () {
+// REMOVE BUTTONS //
+var resetSolved = function() {
     while (answerbuttonEl.firstChild) {
-        answerbuttonEl.removeChild(answerbuttonEl.firstChild);
+        answerbuttonEl.removeChild(answerbuttonEl.firstChild)
     };
 };
 
-// Display question and answer buttons
+// DISPLAY QUESTIONS //
 var displayQuestion = function(index) {
     questionsEl.innerText = index.q
     for (var i = 0; i < index.choices.length; i++) {
-        var answerbtn = document.createElement("button");
-        answerbtn.innerText = index.options[i].choices
-        answerbtn.classList.add("answerbtn");
-        answerbtn.classList.add("btn");
+        var answerbtn = document.createElement('button')
+        answerbtn.innerText = index.choices[i].choice
+        answerbtn.classList.add('btn')
+        answerbtn.classList.add('answerbtn')
         answerbtn.addEventListener("click", checkAnswer)
+        answerbuttonEl.appendChild(answerbtn)
+    }
+};
+
+// CORRECT //
+var correctAnswer = function() {
+    if (correctEl.className = "off") {
+        correctEl.classList.remove("off")
+        correctEl.classList.add("tag")
+        incorrectEl.classList.remove("tag")
+        incorrectEl.classList.add("off")
+    }
+}  
+
+// INCORRECT //
+var incorrectAnswer = function() {
+    if (incorrectEl.className = "off") {
+        incorrectEl.classList.remove("off")
+        incorrectEl.classList.add("tag")
+        correctEl.classList.remove("tag")
+        correctEl.classList.add("off")
     }
 }
 
-// Check to see if it is correct or not //
+// CHECK TO SEE IF ANSWER IS CORRECT OR NOT //
 var checkAnswer = function(event) {
-    var selected = event.target
+    var selected = event.target 
         if (randomQuestions[questionIndex].a === selected.innerText) {
-            correctAnswer();
+            correctAnswer()
             points = points + 8
         } else {
             incorrectAnswer()
-            points = points - 3;
+            points = points - 1;
             timeleft = timeleft - 5;
-
         };
+
+        //
         questionIndex++
-            if (randomQuestions.length > questionIndex + 1) {
-                setQuestion();
-            } else {
-                gameOver = "true";
-                (viewPoints);
-            }
+        if  (randomQuestions.length > questionIndex + 1) {
+            setQuestion()
+        } else {
+            gameover = "true";
+            viewPoints();
+        }
+    
     
 }
 
-// ALERT correct on users screen //
-var correctAnswer = function() {
-    if (correctEl.className = "off") {
-        correctEl.classList.remove("off");
-        correctEl.classList.add("banner");
-        wrongEl.classList.add("off");
-        wrongEl.classList.remove("banner");
-    }
-}
-
-// ALERT incorrect on users screen // 
-var incorrectAnswer = function() {
-    if (wrongEl.className = "off") {
-        wrongEl.classList.remove("off")
-        wrongEl.classList.add("banner")
-        correctEl.classList.add("off")
-        correctEl.classList.remove("banner")
-
-    }
-
-}
-
-// Show points at end game //
+// SHOW POINTS AT END OF GAME //
 var viewPoints = function() {
     boxQuestionEl.classList.add("off");
     boxEndEl.classList.remove("off");
-    boxEndEl.classList.add("show");
+    boxEndEl.classList.add("on");
 
     var pointView = document.createElement("p");
-    pointView.innerText = (" Final score is " + points + "!!!");
-    boxScoreEl.appendChild(pointView);
-}
-
+    pointView.innerText = ("Your final score is " + points + "!");
+    boxHighScoresEl.appendChild(pointView);
+}       
+    
 // HIGH SCORE FUNCTION //
-var savedScore = function(event) {
-    event.preventDefault();
+var savedScore = function(event) { 
+    event.preventDefault() 
     var name = document.querySelector("#name").value;
-    if(!name) {
-        alert("Please enter your name!!!")
+    if (!name) {
+        alert("Please enter your name!");
         return;
     }
 
     nameForm.reset();
 
     var record = {
-        name: name,
-        points: points,
-    }
+      name: name,
+      points: points,
+    } 
 
     records.push(record);
     records.sort((a, b) => {return b.points-a.points});
 
+    while (lsHighScoreEl.firstChild) {
+       lsHighScoreEl.removeChild(lsHighScoreEl.firstChild)
+    }
+    
     for (var i = 0; i < records.length; i++) {
-        var recordsEl = document.createElement("li");
-        records.className = "high-score";
-        records.innerHTML = records[i].name + " - " + records[i].score;
-        highScoreLsEl.appendChild(recordsEl)
-    } while (highScoreLsEl.firstChild) {
-        highScoreLsEl.removeChild(highScoreLsEl.firstChild);
+      var recordsEl = document.createElement("li");
+      recordsEl.className = "high-score";
+      recordsEl.innerHTML = records[i].name + " - " + records[i].points;
+      lsHighScoreEl.appendChild(recordsEl);
     }
 
     saveRecords();
     displayRecords();
-    
+
 }
 
-// saving the records //
+// SAVE RECORDS //
 var saveRecords = function () {
     localStorage.setItem("records", JSON.stringify(records))
+            
 }
 
-// pull records in on the loading of page //
-var loadRecords = function() {
-    var loadedRecords = localStorage.getItem("records");
+// LOAD RECORDS ONTO SCREEN //
+var loadRecords = function () {
+    var loadedRecords = localStorage.getItem("records")
         if (!loadedRecords) {
         return false;
     }
 
-    loadedRecords = JSON.parse(loadedRecords);
-    loadedRecords.sort((a,b) => {return b.points-a.points})
+    records = JSON.parse(loadedRecords);
+    records.sort((a, b) => {return b.points-a.points})
+ 
 
     for (var i = 0; i < loadedRecords.length; i++) {
         var recordsEl = document.createElement("li");
         recordsEl.className = "high-score";
-        recordsEl.innerText = loadedRecords[i].name + " - " +loadedRecords[i].score;
+        recordsEl.innerText = loadedRecords[i].name + " - " + loadedRecords[i].points;
+        lsHighScoreEl.appendChild(recordsEl);
 
         records.push(loadedRecords[i]);
+            
     }
-    
-}
+}  
 
+// DISPLAY RECORDS //
 var displayRecords = function() {
-    boxHighScoreEl.classList.add("on");
-    boxHighScoreEl.classList.remove("off")
-    gameOver = "true"
+
+    boxHighScoresEl.classList.remove("off");
+    boxHighScoresEl.classList.add("on");
+    gameover = "true"
 
     if (boxEndEl.className = "on") {
-        boxEndEl.classList.add("off");
         boxEndEl.classList.remove("on");
-    } 
+        boxEndEl.classList.add("off");
+        }
     if (boxStartEl.className = "on") {
-        boxStartEl.classList.add("off");
         boxStartEl.classList.remove("on");
-    }
+        boxStartEl.classList.add("off");
+        }
+            
     if (boxQuestionEl.className = "on") {
-        boxQuestionEl.classList.add("off");
         boxQuestionEl.classList.remove("on");
-    }
+        boxQuestionEl.classList.add("off");
+        }
+
     if (correctEl.className = "on") {
-        correctEl.classList.add("off");
         correctEl.classList.remove("on");
+        correctEl.classList.add("off");
     }
+
     if (incorrectEl.className = "on") {
-        incorrectEl.classList.add("off");
         incorrectEl.classList.remove("on");
+        incorrectEl.classList.add("off");
     }
 }
 
+// CLEAR RECORDS FUNCTION //
+var recordClear = function() {
+    records = [];
 
-btnStart.addEventListener("click", gameStart);
+    while (lsHighScoreEl.firstChild) {
+        lsHighScoreEl.removeChild(lsHighScoreEl.firstChild);
+    }
 
-nameForm.addEventListener("submit", savedScore);
+    localStorage.clear(records)
+}
 
-viewHighScoreEl.addEventListener("click", displayRecords);
-
-btnBack.addEventListener("click", startPage);
+loadRecords() 
 
 
+        
+// START GAME BUTTON //
+btnStartEl.addEventListener("click", gameStart)
+
+// BACK BTN //
+btnBackEl.addEventListener("click", startPage)
+
+// VIEW RECORDS //
+viewHighScoreEl.addEventListener("click", displayRecords)
+
+// NAME SUBMIT //
+nameForm.addEventListener("submit", savedScore)
+
+// CLEAR BUTTON
+btnClearRecord.addEventListener("click", recordClear)
